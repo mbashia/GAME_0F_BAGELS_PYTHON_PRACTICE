a835a15705bb296422ecaea9caed656571bc17e5
mbashia <mbashiavictor@gmail.com>
2023-02-12 22:52:23 +0300

    second commit

---
+++ bagels.py
@@ -0,0 +1,7 @@
+# in bagels, a deductive logic game, you must guess a secret three-digit number based on clues. The game
+# offers one of the following hints: "Pico" when your guess has a correct digit in the wrong place, "Fermi" when
+# your guess has a correct digit in the correct place and  "bagels" if your guess has no correct values. You have
+# ten tries to guess the secret number.
+import random
+
+def generate_random_num():
